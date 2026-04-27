732ac1c0f33da1da9d1f729c59b3e95062dad141
raz0red <raz0red@gmail.com>
2026-04-26 22:49:31 -0700

    cheater

--- src/pause/index.js
+++ src/pause/index.js
@@ -5,6 +5,8 @@ import { GamepadControlsTab, KeyboardControlsTab } from './controls';
 import { AtariSettingsEditor } from './settings';
 
 import {
+  BoltWhiteImage,
+  CheatsSettingsEditor,
   CustomPauseScreen,
   EditorScreen,
   GamepadWhiteImage,
@@ -30,10 +32,11 @@ export class EmulatorPauseScreen extends Component {
     PAUSE: 'pause',
     CONTROLS: 'controls',
     ATARI_SETTINGS: 'atari-settings',
+    CHEATS: 'cheats',
     STATE: 'state',
   };
 
-  ADDITIONAL_BUTTON_REFS = [React.createRef(), React.createRef(), React.createRef()];
+  ADDITIONAL_BUTTON_REFS = [React.createRef(), React.createRef(), React.createRef(), React.createRef()];
 
   componentDidMount() {
     const { loaded } = this.state;
@@ -96,14 +99,30 @@ export class EmulatorPauseScreen extends Component {
       />
     );
 
+    if (emulator.getCheatsService().getList().length > 0) {
+      additionalButtons.push(
+        <PauseScreenButton
+          imgSrc={BoltWhiteImage}
+          buttonRef={ADDITIONAL_BUTTON_REFS[2]}
+          label="Cheats"
+          onHandlePad={(focusGrid, e) =>
+            focusGrid.moveFocus(e.type, ADDITIONAL_BUTTON_REFS[2])
+          }
+          onClick={() => {
+            this.setState({ mode: ModeEnum.CHEATS });
+          }}
+        />
+      );
+    }
+
     if (cloudEnabled) {
       additionalButtons.push(
         <PauseScreenButton
           imgSrc={SaveWhiteImage}
-          buttonRef={ADDITIONAL_BUTTON_REFS[2]}
+          buttonRef={ADDITIONAL_BUTTON_REFS[3]}
           label={Resources.getText(TEXT_IDS.SAVE_STATES)}
           onHandlePad={(focusGrid, e) =>
-            focusGrid.moveFocus(e.type, ADDITIONAL_BUTTON_REFS[2])
+            focusGrid.moveFocus(e.type, ADDITIONAL_BUTTON_REFS[3])
           }
           onClick={() => {
             this.setState({ mode: ModeEnum.STATE });
@@ -150,6 +169,12 @@ export class EmulatorPauseScreen extends Component {
 
         {mode === ModeEnum.ATARI_SETTINGS ? (
           <AtariSettingsEditor
+            emulator={emulator}
+            onClose={closeCallback}
+          />
+        ) : null}
+        {mode === ModeEnum.CHEATS ? (
+          <CheatsSettingsEditor
             emulator={emulator}
             onClose={closeCallback}
           />
